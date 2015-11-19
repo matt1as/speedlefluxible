5e26bfa55d75d6204e3285c4016c56e3f2274fd6
Mattias Johansson <mattias.e.johansson@gmail.com>
2015-11-20 00:20:26 +0100

    Added navbar

---
+++ components/NavBar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { NavLink } from 'fluxible-router';
+import { NavItem } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+
+
+
+
+class NavBar extends React.Component {
+
+
+
+    render() {
+        const selected = this.props.selected;
+        const links = this.props.links;
+
+
+        const linkHTML = Object.keys(links).map((name) => {
+            var className = '';
+            var link = links[name];
+
+            if (selected === name) {
+                className = 'pure-menu-selected';
+            }
+
+            var li;
+            if ( link.inMenu == true ) {
+              li =   <li className={className} key={link.path}>
+                    <NavLink routeName={link.page} activeStyle={{backgroundColor: '#eee'}}>{link.title}</NavLink>
+                </li>
+            } else {
+              li = '';
+            }
+
+            return (
+              <span>{li}</span>
+            );
+        });
+
+        const linkNav = Object.keys(links).map((name => {
+          var link = links[name];
+          if(link.inMenu ) {
+            return <NavItem eventKey={1} href={link.path}>{link.title}</NavItem>
+          } else { return null };
+        }))
+
+
+        return (
+          <Nav bsStyle="pills" activeKey={1} >
+            {linkNav}
+          </Nav>
+
+
+        );
+    }
+
+}
+
+NavBar.defaultProps = {
+    selected: 'home',
+    links: {}
+};
+
+export default NavBar;
